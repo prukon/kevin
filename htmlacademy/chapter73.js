f02454734a0aef191f50845b9e794d32557a8bf4
prukon <prukon@gmail.com>
2018-05-02 01:06:04 +0300

    дешифровик Цезаря (fix)

--- htmlacademy/chapter73.js
+++ htmlacademy/chapter73.js
@@ -1,23 +1,24 @@
-//дешифровик Цезаря
-
 // Алфавит
 var symbols = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ', '.', ',', '—', '!'];
-
+// console.log(symbols.length);
 // Смещение
 var shift = 10;
+var newsimvol = 0;
 // Закодированное сообщение
 var encodedMessage = [8, 28, 36, 52, 56, 40, 23, 31, 56, 39, 38, 28, 48, 52, 58, 56, 38, 27, 32, 37, 56, 40, 23, 31, 56, 39, 38, 41, 39, 32, 57];
 
-//дефифратор
+// Раскодированное сообщение
 var decodedMessage = '';
 for (var i = 0; i <= (encodedMessage.length - 1); i++) {
-    // console.log("Номер "+ i, encodedMessage[i]);
+    if (((encodedMessage[i] + shift) >= (symbols.length))) {
+        decodedMessage += (symbols[(encodedMessage[i] + shift) - (symbols.length)]);
+        console.log((symbols[(encodedMessage[i] + shift) - (symbols.length)]));
+    }
     for (var n = 0; n <= (symbols.length - 1); n++) {
-        if ((encodedMessage[i] + shift) === n ) {
-            // console.log("буква "+ n, symbols[n]);
+        if (((encodedMessage[i] + shift) === n)
+        ) {
             decodedMessage += symbols[n];
         }
     }
 }
-// Раскодированное сообщение
 console.log(decodedMessage);
